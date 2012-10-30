f55e316d77c5524d4d30ddf4aecd1a73ce62d5ff
mila <kamilpierzchalski@gmail.com>
2012-10-31 00:22:40 +0100

    Dodalem szablon formularza dodawania eventu. Potrzebuje jeszce kilku poprawek.

--- war/js/main.js
+++ war/js/main.js
@@ -1,1 +1,7 @@
-
+$(function(){
+	$(".details_button").css("cursor","pointer");
+	$(".details").css("display","none");
+	$(".details_button").click(function(){
+		$(".details").slideToggle();
+	});
+});
