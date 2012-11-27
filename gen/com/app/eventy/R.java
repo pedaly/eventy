5388e0521f3709db815425578683593d2443e2aa
Mariusz Kopacz <mariusz_enter@wp.pl>
2012-11-27 21:10:04 +0100

    porzadki po problemach

--- gen/com/app/eventy/R.java
+++ gen/com/app/eventy/R.java
@@ -16,54 +16,48 @@ public final class R {
         public static final int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static final int androidmarker=0x7f020000;
-        public static final int event_marker=0x7f020001;
-        public static final int ic_action_search=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int event_marker=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int imprezyCheckBox=0x7f08001d;
-        public static final int imprezyId=0x7f08001e;
-        public static final int imprezyOkolicznoscioweCheckBox=0x7f08001b;
-        public static final int imprezyOkolicznoscioweId=0x7f08001c;
-        public static final int kategorieId=0x7f08000b;
-        public static final int kategorieOpisId=0x7f08000c;
-        public static final int kinoCheckBox=0x7f08000f;
-        public static final int kinoId=0x7f080010;
-        public static final int koncertyCheckBox=0x7f08000d;
-        public static final int koncertyId=0x7f08000e;
-        public static final int mapview=0x7f080005;
-        public static final int menu_settings=0x7f080020;
-        public static final int number=0x7f08000a;
-        public static final int pokazyCheckBox=0x7f080017;
-        public static final int pokazyId=0x7f080018;
-        public static final int promiecId=0x7f080007;
-        public static final int promienOpisId=0x7f080008;
-        public static final int saveSettings=0x7f08001f;
-        public static final int scrollView1=0x7f080006;
-        public static final int seekBar=0x7f080009;
-        public static final int spektakleCheckBox=0x7f080013;
-        public static final int spektakleId=0x7f080014;
-        public static final int targiCheckBox=0x7f080019;
-        public static final int targiId=0x7f08001a;
-        public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080001;
-        public static final int textView3=0x7f080003;
-        public static final int textView4=0x7f080004;
-        public static final int title=0x7f080000;
-        public static final int wydarzeniaId=0x7f080016;
-        public static final int wydarzeniaeCheckBox=0x7f080015;
-        public static final int wystawyCheckBox=0x7f080011;
-        public static final int wystawyId=0x7f080012;
+        public static final int imprezyCheckBox=0x7f07001d;
+        public static final int imprezyId=0x7f07001e;
+        public static final int imprezyOkolicznoscioweCheckBox=0x7f07001b;
+        public static final int imprezyOkolicznoscioweId=0x7f07001c;
+        public static final int kategorieId=0x7f07000b;
+        public static final int kategorieOpisId=0x7f07000c;
+        public static final int kinoCheckBox=0x7f07000f;
+        public static final int kinoId=0x7f070010;
+        public static final int koncertyCheckBox=0x7f07000d;
+        public static final int koncertyId=0x7f07000e;
+        public static final int mapview=0x7f070005;
+        public static final int number=0x7f07000a;
+        public static final int pokazyCheckBox=0x7f070017;
+        public static final int pokazyId=0x7f070018;
+        public static final int promiecId=0x7f070007;
+        public static final int promienOpisId=0x7f070008;
+        public static final int saveSettings=0x7f07001f;
+        public static final int scrollView1=0x7f070006;
+        public static final int seekBar=0x7f070009;
+        public static final int spektakleCheckBox=0x7f070013;
+        public static final int spektakleId=0x7f070014;
+        public static final int targiCheckBox=0x7f070019;
+        public static final int targiId=0x7f07001a;
+        public static final int textView1=0x7f070002;
+        public static final int textView2=0x7f070001;
+        public static final int textView3=0x7f070003;
+        public static final int textView4=0x7f070004;
+        public static final int title=0x7f070000;
+        public static final int wydarzeniaId=0x7f070016;
+        public static final int wydarzeniaeCheckBox=0x7f070015;
+        public static final int wystawyCheckBox=0x7f070011;
+        public static final int wystawyId=0x7f070012;
     }
     public static final class layout {
         public static final int activity_event=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_settings=0x7f030002;
-    }
-    public static final class menu {
-        public static final int activity_main=0x7f070000;
-        public static final int activity_settings=0x7f070001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
