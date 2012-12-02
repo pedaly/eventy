84415f2f380f725501dd3d2dd974327b35a2f816
Magdalena Ciężka <magdalena.ciezka@gmail.com>
2012-12-03 00:09:10 +0100

    security cd

--- src/pl/nonamesevent/controller/IndexController.java
+++ src/pl/nonamesevent/controller/IndexController.java
@@ -1,8 +1,14 @@
 package pl.nonamesevent.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import pl.nonamesevent.dao.Dao;
+import pl.nonamesevent.model.Event;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -11,13 +17,21 @@ import com.google.appengine.api.users.UserServiceFactory;
 public class IndexController {
 
 	@RequestMapping(value = { "/index", "/" }, method = RequestMethod.GET)
-	public String index() {
+	public ModelAndView index() {
 	 	UserService userService = UserServiceFactory.getUserService();
+	 
+	 	//w mojej jebanej przegladrce nie chce dzialac redirect i sie pluje o jakies ciasteczka ;/
 		
 		if( userService.getCurrentUser() != null ){
-			return "eventsList";
-		}else
-			return "index";
+			ModelAndView mav = new ModelAndView("eventsList");
+			List<Event> events = Dao.INSTANCE.getEvents();
+			mav.addObject("events", events);
+			return mav;
+		}else{
+			ModelAndView mav = new ModelAndView("index");
+			return mav;
+		}
+			
 		
 	}
 }
--- war/WEB-INF/jsp/index.jsp
+++ war/WEB-INF/jsp/index.jsp
@@ -25,7 +25,7 @@
 <body>
 
 	<div class="mainPage">
-		<a href="/eventsList"><div class="logoMain"></div></a>
+		<div class="logoMain"></div>
 		<div>
 			<p>
 				<%
