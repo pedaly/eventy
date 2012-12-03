f44f68ca6f3b0d2f241143e104f0e9afe70540e5
Magdalena Ciężka <magdalena.ciezka@gmail.com>
2012-12-03 22:25:56 +0100

    main page redirect

--- src/pl/nonamesevent/controller/IndexController.java
+++ src/pl/nonamesevent/controller/IndexController.java
@@ -1,14 +1,10 @@
 package pl.nonamesevent.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import pl.nonamesevent.dao.Dao;
-import pl.nonamesevent.model.Event;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -22,16 +18,13 @@ public class IndexController {
 	 
 	 	//w mojej jebanej przegladrce nie chce dzialac redirect i sie pluje o jakies ciasteczka ;/
 		
-		if( userService.getCurrentUser() != null ){
-			ModelAndView mav = new ModelAndView("eventsList");
-			List<Event> events = Dao.INSTANCE.getEvents();
-			mav.addObject("events", events);
+		if (userService.getCurrentUser() != null) {
+			ModelAndView mav = new ModelAndView(new RedirectView("/eventsList"));
 			return mav;
-		}else{
+		} else {
 			ModelAndView mav = new ModelAndView("index");
 			return mav;
 		}
-			
 		
 	}
 }
